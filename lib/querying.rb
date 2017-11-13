e27545e299eff24bb20c8983715400bdfc6a3ca9
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-13 20:23:40 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -17,7 +17,7 @@ def select_value_and_count_of_most_prolific_species
   "SELECT species, COUNT(DISTINCT species)
   FROM characters
   ORDER BY COUNT(DISTINCT species) DESC
-  LIMIT 1"
+  "
 end
 
 def select_name_and_series_subgenres_of_authors
