87963aba8ac0d267d08e8f32f92ba6af0ecce1d9
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-13 20:30:37 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,7 +23,9 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  ""
+  "SELECT authors.name, series.subgenre_id
+  FROM authors JOIN series WHERE authors.id = series.author_id
+  GROUP BY authors.name"
 end
 
 def select_series_title_with_most_human_characters
